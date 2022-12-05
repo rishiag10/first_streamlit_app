34e54f254eb4c5cebd32305d7cee04be6806fbfa
rishiag10 <rishi10978@gmail.com>
2022-12-05 16:25:29 +0530

    Update streamlit_app.py
    Fixed Header to Text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Parents New Healithy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Veggie Sandwich')
-streamlit.header('Corn Salad')
-streamlit.header('Kale and Spinach Salad')
-streamlit.header('Orange juice')
-streamlit.header('Coffee')
+streamlit.text('Corn Salad')
+streamlit.text('Kale and Spinach Salad')
+streamlit.text('Orange juice')
+streamlit.text('Coffee')
 
