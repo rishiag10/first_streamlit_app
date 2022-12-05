61c90cab193e92a39fde2549d63679108509bc38
rishiag10 <rishi10978@gmail.com>
2022-12-05 16:24:58 +0530

    Update streamlit_app.py
    Added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
-streamlit.title('My Parent New Healithy Diner')
+streamlit.title('My Parents New Healithy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Veggie Sandwich')
+streamlit.header('Corn Salad')
+streamlit.header('Kale and Spinach Salad')
+streamlit.header('Orange juice')
+streamlit.header('Coffee')
+
