223e9f56fc422da844df88e0fda2893561fc3bd7
rishiag10 <rishi10978@gmail.com>
2022-12-10 15:05:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import requests
+#import connector
+import snowflake.connector
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -36,5 +38,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-#import connector
-import snowflake.connector
+
